0d9b32c2a9415fe5dd96610568b1b5773ba3aa24
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-07-28 12:26:55 -0400

    Add new methods to 'stringUtils'.

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -9,10 +9,18 @@ function handleize( str ) {
     return str;
   }
 
-  console.log( 'INSIDE `handleize` --> LOGGING OUT `str`' ); /// TEMP
-  console.log( str ); /// TEMP
   str = str.replace( / /gmi, '-' );
   str = str.toLowerCase();
+
+  return str;
+}
+
+function dehandleize( str ) {
+   if ( !str || typeof str !== 'string' ) {
+    return str;
+  }
+
+  str = str.replace( /\-/gmi, ' ' );
 
   return str;
 }
@@ -31,10 +39,41 @@ function urlize( str ) {
   return str;
 }
 
+/**
+ * ...
+ */
+function titleize( str ) {
+  if ( !str || typeof str !== 'string' ) {
+    return str;
+  }
+
+  return str.split( ' ' )
+    .map( ( str ) => {
+      return `${str.substring( 0, 1 ).toUpperCase()}${str.substring( 1 ).toLowerCase()}`
+    } )
+    .join( ' ' );
+}
+
+/**
+ * ...
+ */
+function transform( str, callbacks ) {
+  if ( !str || typeof str !== 'string' ) {
+    return str;
+  }
+
+  return callbacks.reduce( ( str, callback ) => {
+    return ( callback && typeof callback === 'function' ) ? callback( str ) : str;
+  }, str );
+}
+
 // --------------------------------------------------
 // PUBLIC API
 // --------------------------------------------------
 export const stringUtils = {
   handleize,
+  dehandleize,
   urlize,
+  titleize,
+  transform,
 };
